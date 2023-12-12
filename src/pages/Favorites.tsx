0556ba8d82576779f39f2f3e1d5636a6d4de9336
Karen Almeida <karenalmeida.lukiany@gmail.com>
2023-12-12 10:57:50 +0000

    Cria requisito 8

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from 'react';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import MusicCard from '../components/MusicCard';
+import { SongType } from '../types';
+
 function Favorites() {
+  const [favoritsSongs, setFavoritsSongs] = useState<SongType[]>([]);
+  const [favoritsLength, setFavoritsLength] = useState(0);
+
+  useEffect(() => {
+    const configFavoritesSongs = async () => {
+      const favorits = await getFavoriteSongs();
+      console.log(favorits);
+
+      if (favorits.length !== favoritsLength) {
+        setFavoritsSongs(favorits);
+        setFavoritsLength(favoritsSongs.length);
+      }
+    };
+    configFavoritesSongs();
+  }, [favoritsSongs, favoritsLength]);
+
+  // if (favoritsSongs.length !== favoritsLength) {
+
+  // }
+
   return (
-    <div>Favorites</div>
+    <div>
+      {favoritsSongs && favoritsSongs.map((music, index) => (
+        <div key={ index }>
+          <MusicCard
+            musicInfo={ music }
+          />
+        </div>
+      ))}
+    </div>
   );
 }
 
